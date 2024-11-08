9100c80cdec8628b62e34c01c34f1a98b2d49907
benslack19 <ben.lacar@gmail.com>
2024-11-07 20:07:27 -0800

    fix: assess ruff formatting in nb and script

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 """Common data science utility functions"""
 
-import numpy as np
 from typing import List, Union
+
+import numpy as np
+import pandas as pd
+
+RANDOM_SEED = 12345
 
 
 def standardize(x: Union[List, np.array]):
@@ -9,11 +13,13 @@ def standardize(x: Union[List, np.array]):
     return x
 
 
-fruits = [
-    "apple", "banana", "orange", "apple",
-    "banana",
-    "orange",
-    "apple",
-    "banana",
-    "orange",
-]
+# df with continuous, ordinal, and binary variables
+nrows, ncols = 100, 5
+rng = np.random.default_rng(RANDOM_SEED)
+
+df_example = pd.DataFrame(
+    rng.rand(nrows, ncols), columns=["a", "b", "c", "d", "e"]
+).assign(
+    f_ordinal=rng.choice([0, 1, 2, 3], nrows),
+    g_binary=rng.choice([True, False], nrows),
+)
