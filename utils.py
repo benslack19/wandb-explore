a56a898c24e76f03c019de0209aa17fbc128df06
benslack19 <ben.lacar@gmail.com>
2024-11-18 10:25:23 -0800

    style: add mypy and edit precommit

--- utils.py
+++ utils.py
@@ -1,25 +1,16 @@
 """Common data science utility functions"""
 
-from typing import List, Union
+from typing import List, TypeAlias, Union
 
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 
-RANDOM_SEED = 12345
+NumericData: TypeAlias = Union[List[float], List[int], npt.ArrayLike, pd.Series]
 
 
-def standardize(x: Union[List, np.array]):
-    x = (x - np.mean(x)) / np.std(x)
-    return x
-
-
-# df with continuous, ordinal, and binary variables
-nrows, ncols = 100, 5
-rng = np.random.default_rng(RANDOM_SEED)
-
-df_example = pd.DataFrame(
-    rng.rand(nrows, ncols), columns=["a", "b", "c", "d", "e"]
-).assign(
-    f_ordinal=rng.choice([0, 1, 2, 3], nrows),
-    g_binary=rng.choice([True, False], nrows),
-)
+def standardize(x: NumericData) -> npt.NDArray[np.floating]:
+    arr = np.asarray(x, dtype=np.floating)
+    # mean: np.float64 = np.mean(arr)
+    # std: np.float64 = np.std(arr)
+    return (arr - np.mean(arr)) / np.std(arr)
